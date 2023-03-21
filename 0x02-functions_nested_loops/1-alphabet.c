93a7761307ff6590069b052887da4f1be6cb44df
Brenda <brendakiseu17@gmail.com>
2023-03-21 03:00:50 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Write the alphabets
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
