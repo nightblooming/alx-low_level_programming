3a7a6ab14179d4c4b8201ddeae786c5368286426
Brenda <brendakiseu17@gmail.com>
2023-03-23 00:05:54 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - checks whether the character is upper case
+ * @c: character to be checked
+ * Return: 1 if c is uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return(1);
+	}
+	return(0);
+}
