61a964272cb6fc031f3fa4762ef29b5d0cb025f0
Brenda <brendakiseu17@gmail.com>
2023-03-23 06:23:55 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,13 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 1; x <= 10; x++)
+	for (x = 0; x < 10; x++)
 	{
 	for (y = 0; y <= 14; y++)
 	{
-		if (y >= 10)
+		if (y > 9)
 		{
-			_putchar('x');
+			_putchar((y / 10) + '0');
 		}
 		_putchar((y % 10) + '0');
 	}
