a0aac0ba10c443497a52aba29a8588755eb755f4
Brenda <brendakiseu17@gmail.com>
2023-03-27 12:34:59 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - copy a string
+ * @dest: Destination value
+ * @src: source of value
+ * Return: the pointer to destroy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
