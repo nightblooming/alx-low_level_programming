9bbab98fc001f41c7f4958c3778ee4020f0b305b
“Brenda” <brendakiseu17@gmail.com>
2023-05-02 11:51:59 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,15 +2,17 @@
 
 /**
  * free_listint - frees listint_t list
+ *
  * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	do {
+	while (head)
+	{
 		temp = head->next;
 		free(head);
 		head = temp;
-	} while (head != NULL);
+	}
 }
